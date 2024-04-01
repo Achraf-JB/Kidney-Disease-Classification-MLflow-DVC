940cafb622c4b0c51bde8ade08edaa959fabc26e
achref <jarbouiachraf899@gmail.com>
2024-04-01 18:35:54 +0200

    model_evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_03_training_model  import ModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_04_model_evaluation  import ModelEvaluationPipeline
 # STAGE_NAME = "Data Ingestion stage"
 # try:
 #    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -21,10 +22,22 @@ from src.cnnClassifier.pipeline.stage_03_training_model  import ModelTrainingPip
 # except Exception as e:
 #         logger.exception(e)
 #         raise e
-STAGE_NAME = "Data Ingestion stage"
+# STAGE_NAME = "Model training stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    model_trainig = ModelTrainingPipeline()
+#    model_trainig.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+
+
+STAGE_NAME = " Model Evaluation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   model_trainig = ModelTrainingPipeline()
+   model_trainig = ModelEvaluationPipeline()
    model_trainig.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
